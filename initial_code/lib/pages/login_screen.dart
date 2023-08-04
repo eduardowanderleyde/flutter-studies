d566bf59863959b888c956cae9c1694450bfe2db
Eduardo Wanderley de Siqueira Araújo <103546791+eduardowanderleyde@users.noreply.github.com>
2023-08-04 13:42:05 -0300

    adjusting sizes

--- initial_code/lib/pages/login_screen.dart
+++ initial_code/lib/pages/login_screen.dart
@@ -3,23 +3,21 @@ import 'package:initial_code/pages/register_sceen.dart';
 import 'package:initial_code/pages/task_list.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('App'),
-      ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset('assets/img/1.png', width: 500, height: 500),
+              Expanded(
+                child: Image.asset('assets/img/1.png'),
+              ),
               const SizedBox(height: 10),
               LoginCard(),
             ],
@@ -35,91 +33,93 @@ class LoginCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  final TextEditingController usernameController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+    final TextEditingController usernameController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
 
-void handleLoginButtonPressed() async {
-  String username = usernameController.text;
-  String password = passwordController.text;
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String? storedUsername = prefs.getString('username');
-  String? storedPassword = prefs.getString('password');
-  print(username);
-  print(storedUsername);
+    void handleLoginButtonPressed() async {
+      String username = usernameController.text;
+      String password = passwordController.text;
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? storedUsername = prefs.getString('username');
+      String? storedPassword = prefs.getString('password');
+      print(username);
+      print(storedUsername);
 
-  if (username == storedUsername && password == storedPassword) {
-    // Username and password match the ones stored in SharedPreferences
-    // You can add additional logic here if needed.
-    print("oi"); // For demonstration purposes, we'll print a message.
-  } else {
-    // Username or password is incorrect
-    // You can add additional logic here, such as showing an error message, etc.
-    print('Username or password is incorrect. Please try again.'); // For demonstration purposes, we'll print a message.
-  }
-}
+      if (username == storedUsername && password == storedPassword) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const TaskList()),
+        );
+        // Username and password match the ones stored in SharedPreferences
+        // You can add additional logic here if needed.
+        print("oi"); // For demonstration purposes, we'll print a message.
+      } else {
+        // Username or password is incorrect
+        // You can add additional logic here, such as showing an error message, etc.
+        print('ae'); // For demonstration purposes, we'll print a message.
+      }
+    }
 
-
-
-    return Card(
-      color: const Color(0xFF00FF00), // Utilizando um valor hexadecimal (0xFF00FF00 - verde)
-      elevation: 4,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-             Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextField(
-                controller: usernameController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  labelText: 'Username',
-                  border: OutlineInputBorder()
+    return SizedBox(
+      width: 500,
+      height: 300,
+      child: Card(
+        color: const Color(0xFF00FF00), // Utilizando um valor hexadecimal (0xFF00FF00 - verde)
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: usernameController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-             Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  border:OutlineInputBorder()
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                handleLoginButtonPressed();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const TaskList()),
-                );
-              },
-              child: const Text('Enviar'),
-            ),
-            const SizedBox(height: 10), // Espaço entre os botões (ElevatedButton
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
+              ElevatedButton(
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const RegisterScreen()),
-                  );
+                  handleLoginButtonPressed();
                 },
-                style: ElevatedButton.styleFrom(
-                  primary: const Color(0xFFFFFF), // Exemplo de outra cor (0xFF123456 - roxo)
-                ),
-                child: const Text('Register'),
+                child: const Text('Enviar'),
               ),
-            ),
-          ],
+              const SizedBox(height: 5), // Espaço entre os botões (ElevatedButton)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const RegisterScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color(0xFFFFFF), // Exemplo de outra cor (0xFF123456 - roxo)
+                  ),
+                  child: const Text('Register'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
